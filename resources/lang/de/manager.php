6b3d30f84d97cc6179928158c9b8a29b9080843c
grafst <grafst@users.noreply.github.com>
2024-09-13 14:49:49 +0200

    translate to german

---
+++ resources/lang/de/manager.php
@@ -0,0 +1,30 @@
+<?php
+
+// translations for Kenepa/Banner
+return [
+    'title' => 'Banner Verwaltung',
+    'subheading' => 'Verwalte deine Banner',
+    'navigation_label' => 'Banner Verwaltung',
+    'create' => 'Erstellen',
+    'save' => 'Speichern',
+    'inactive' => 'Inaktiv',
+    'banner_list_empty_state_title' => 'Noch keine Banner',
+    'banner_list_empty_state_description' => 'Sobald du dein erstes Banner erstellst, wird es hier angezeigt.',
+    'banner_edit_empty_state_title' => 'Kein Banner ausgewählt',
+    'banner_edit_empty_state_description' => 'Wähle oder erstelle ein Banner, um loszulegen',
+    'enable_all_banners' => 'Alle Banner aktivieren',
+    'disable_all_banners' => 'Alle Banner deaktivieren',
+    'successfully_deleted_banner' => 'Banner gelöscht',
+    'successfully_updated_banner' => 'Banner aktualisiert',
+    'successfully_created_banner' => 'Banner erstellt',
+    'disabled_all_banners' => 'Alle Banner deaktiviert',
+    'enabled_all_banners' => 'Alle Banner aktiviert',
+    'failed_to_load_banner' => 'Banner konnte nicht geladen werden',
+    'banner_name' => 'Name',
+    'banner_content' => 'Inhalt',
+    'banner_render_location' => 'Anzeigeposition',
+    'status_due' => 'Fällig',
+    'status_visible' => 'Sichtbar',
+    'status_fulfilled' => 'Erfüllt',
+    'active_since' => 'Aktiv seit',
+];
